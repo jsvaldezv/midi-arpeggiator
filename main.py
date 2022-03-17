d6171ef2c81e57e7f3a63760e23c2496c90a7064
Jesús Valdez <jsvaldezv@yahoo.com>
2022-03-16 21:49:15 -0600

    Arp first

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#################################################### IMPORTS ################################################
+#################################################### IMPORTS ##################################################
 from midiutil import MIDIFile
 import utilities
 
@@ -17,18 +17,22 @@ for i in range(numNotas):
 	nota = input("Nota "+ str(i+1) + " (en notación musical): ")
 	notas.append(utilities.getMidiFromNote(nota))
 
-#################################################### MIDI PROCESSING ################################################
+#################################################### MIDI PROCESSING ##########################################
 midiFile = MIDIFile(1)
 midiFile.addTempo(0, 0, bpm)
 
 tiempo = 0
 duration = utilities.getDurationFromNotation(rate)
+intervalTimes = 4/duration
 
 for i in range(numNotas):
-	utilities.addNote(midiFile, notas[i], 127, tiempo, duration)
-	tiempo += duration	
+	nota = notas[i]
+	for j in range(int(intervalTimes)):
+		utilities.addNote(midiFile, nota, 127, tiempo, duration)
+		tiempo += duration
+		nota += distance
 
-################################################# GENERATE MIDI #################################################
+################################################# GENERATE MIDI ###############################################
 with open("Arp.mid", "wb") as myOutputMIDIClip:
     midiFile.writeFile(myOutputMIDIClip)
 
