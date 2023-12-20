79c48349319e39169b954abffb1450da3469135a
Jesús Valdez <jsvaldezv@gmail.com>
2023-12-20 00:27:51 -0600

    Apply black

--- main.py
+++ main.py
@@ -10,256 +10,260 @@ from pretty_midi import PrettyMIDI
 print("-------------------")
 print("Arpeggiator")
 
+
 class Main(QMainWindow, QWidget):
+    def __init__(self):
+        super().__init__()
+        self.resize(500, 530)
 
-	def __init__(self):
-		super().__init__()
-		self.resize(500, 530)
+        # VARIABLES GLOBALES #
+        self.notas = []
+        self.octavas = []
+        self.notasEnMidi = []
 
-		# VARIABLES GLOBALES #
-		self.notas = []
-		self.octavas = []
-		self.notasEnMidi = []
+        self.createComponents()
 
-		self.createComponents()
+    def createComponents(self):
+        # BPM #
+        self.bpmLabel = QLabel(self)
+        self.bpmLabel.setText("BPM")
+        self.bpmLabel.setGeometry(15, 10, 110, 30)
 
-	def createComponents(self):
-		# BPM #
-		self.bpmLabel = QLabel(self)
-		self.bpmLabel.setText("BPM")
-		self.bpmLabel.setGeometry(15, 10, 110, 30)
+        self.bpm = QSpinBox(self)
+        self.bpm.setGeometry(15, 35, 110, 30)
+        self.bpm.setValue(85)
+        self.bpm.setRange(60, 210)
 
-		self.bpm = QSpinBox(self)
-		self.bpm.setGeometry(15, 35, 110, 30)
-		self.bpm.setValue(85)
-		self.bpm.setRange(60, 210)
+        # KEY #
+        self.keyLabel = QLabel(self)
+        self.keyLabel.setText("Key")
+        self.keyLabel.setGeometry(15, 70, 110, 30)
 
-		# KEY #
-		self.keyLabel = QLabel(self)
-		self.keyLabel.setText("Key")
-		self.keyLabel.setGeometry(15, 70, 110, 30)
-		
-		self.keyCombo = QComboBox(self)
-		self.keyCombo.setGeometry(10, 95, 110, 30)
+        self.keyCombo = QComboBox(self)
+        self.keyCombo.setGeometry(10, 95, 110, 30)
 
-		for key in utilities.keys: 
-			self.keyCombo.addItem(key)
+        for key in utilities.keys:
+            self.keyCombo.addItem(key)
 
-		# MODO #
-		self.modoLabel = QLabel(self)
-		self.modoLabel.setText("Modo")
-		self.modoLabel.setGeometry(15, 125, 110, 30)
-		
-		self.modoCombo = QComboBox(self)
-		self.modoCombo.setGeometry(10, 150, 110, 30)
+        # MODO #
+        self.modoLabel = QLabel(self)
+        self.modoLabel.setText("Modo")
+        self.modoLabel.setGeometry(15, 125, 110, 30)
 
-		for modo in utilities.modos: 
-			self.modoCombo.addItem(modo)
+        self.modoCombo = QComboBox(self)
+        self.modoCombo.setGeometry(10, 150, 110, 30)
 
-		# ORDER #
-		self.orderLabel = QLabel(self)
-		self.orderLabel.setText("Style")
-		self.orderLabel.setGeometry(15, 180, 110, 30)
-		
-		self.orderCombo = QComboBox(self)
-		self.orderCombo.setGeometry(10, 205, 110, 30)
+        for modo in utilities.modos:
+            self.modoCombo.addItem(modo)
 
-		for order in utilities.orders: 
-			self.orderCombo.addItem(order)
+        # ORDER #
+        self.orderLabel = QLabel(self)
+        self.orderLabel.setText("Style")
+        self.orderLabel.setGeometry(15, 180, 110, 30)
 
-		# DISTANCE #
-		self.distanceLabel = QLabel(self)
-		self.distanceLabel.setText("Distancia")
-		self.distanceLabel.setGeometry(15, 235, 110, 30)
-		
-		self.distanceCombo = QSpinBox(self)
-		self.distanceCombo.setGeometry(15, 260, 110, 30)
-		self.distanceCombo.setValue(2)
-		self.distanceCombo.setRange(-12, 12)
+        self.orderCombo = QComboBox(self)
+        self.orderCombo.setGeometry(10, 205, 110, 30)
 
-		# RATE #
-		self.rateLabel = QLabel(self)
-		self.rateLabel.setText("Rate")
-		self.rateLabel.setGeometry(15, 290, 110, 30)
-		
-		self.rateCombo = QComboBox(self)
-		self.rateCombo.setGeometry(10, 315, 110, 30)
+        for order in utilities.orders:
+            self.orderCombo.addItem(order)
 
-		for duration in utilities.durations:
-			self.rateCombo.addItem(duration)
+        # DISTANCE #
+        self.distanceLabel = QLabel(self)
+        self.distanceLabel.setText("Distancia")
+        self.distanceLabel.setGeometry(15, 235, 110, 30)
 
-		# STEPS #
-		self.stepsLabel = QLabel(self)
-		self.stepsLabel.setText("Steps")
-		self.stepsLabel.setGeometry(15, 345, 110, 30)
-		
-		self.stepsCombo = QSpinBox(self)
-		self.stepsCombo.setGeometry(15, 370, 110, 30)
-		self.stepsCombo.setValue(2)
-		self.stepsCombo.setRange(1, 20)
+        self.distanceCombo = QSpinBox(self)
+        self.distanceCombo.setGeometry(15, 260, 110, 30)
+        self.distanceCombo.setValue(2)
+        self.distanceCombo.setRange(-12, 12)
 
-		# NUM DE NOTAS #
-		self.numNotasLabel = QLabel(self)
-		self.numNotasLabel.setText("Num de notas")
-		self.numNotasLabel.setGeometry(15, 405, 110, 30)
-		
-		self.numNotasCombo = QSpinBox(self)
-		self.numNotasCombo.setGeometry(15, 430, 110, 30)
-		self.numNotasCombo.setValue(2)
-		self.numNotasCombo.setRange(1, 20)
+        # RATE #
+        self.rateLabel = QLabel(self)
+        self.rateLabel.setText("Rate")
+        self.rateLabel.setGeometry(15, 290, 110, 30)
 
-		# NUM DE COMPASES #
-		self.numCompasesLabel = QLabel(self)
-		self.numCompasesLabel.setText("Num de compases")
-		self.numCompasesLabel.setGeometry(15, 465, 110, 30)
-		
-		self.numCompasesCombo = QSpinBox(self)
-		self.numCompasesCombo.setGeometry(15, 490, 120, 30)
-		self.numCompasesCombo.setValue(2)
-		self.numCompasesCombo.setRange(1, 20)
+        self.rateCombo = QComboBox(self)
+        self.rateCombo.setGeometry(10, 315, 110, 30)
 
-		# READY #
-		self.btnReady = QPushButton('Ready', self)
-		self.btnReady.setGeometry(140, 10, 100, 45)
-		self.btnReady.clicked.connect(lambda: self.inputNotes())
+        for duration in utilities.durations:
+            self.rateCombo.addItem(duration)
 
-		self.userNotification = QLabel(self)
-		self.userNotification.setText("Hay notas que no forman parte")
-		self.userNotification.setGeometry(310, 50, 200, 30)
-		self.userNotification.hide()
-		self.userNotificationTwo = QLabel(self)
-		self.userNotificationTwo.setText("de la escala")
-		self.userNotificationTwo.setGeometry(310, 70, 200, 30)
-		self.userNotificationTwo.hide()
+        # STEPS #
+        self.stepsLabel = QLabel(self)
+        self.stepsLabel.setText("Steps")
+        self.stepsLabel.setGeometry(15, 345, 110, 30)
 
-	def clear(self):
-		for object in self.notas:
-			object.hide()
-		for object in self.octavas:
-			object.hide()
+        self.stepsCombo = QSpinBox(self)
+        self.stepsCombo.setGeometry(15, 370, 110, 30)
+        self.stepsCombo.setValue(2)
+        self.stepsCombo.setRange(1, 20)
 
-		self.notas.clear()
-		self.octavas.clear()
+        # NUM DE NOTAS #
+        self.numNotasLabel = QLabel(self)
+        self.numNotasLabel.setText("Num de notas")
+        self.numNotasLabel.setGeometry(15, 405, 110, 30)
 
-	def inputNotes(self):
-		self.userNotification.hide()
-		self.userNotificationTwo.hide()
+        self.numNotasCombo = QSpinBox(self)
+        self.numNotasCombo.setGeometry(15, 430, 110, 30)
+        self.numNotasCombo.setValue(2)
+        self.numNotasCombo.setRange(1, 20)
 
-		self.clear()
-		yInitChecBox = 130
+        # NUM DE COMPASES #
+        self.numCompasesLabel = QLabel(self)
+        self.numCompasesLabel.setText("Num de compases")
+        self.numCompasesLabel.setGeometry(15, 465, 110, 30)
 
-		self.notasLabel = QLabel(self)
-		self.notasLabel.setText("Notas fundamentales")
-		self.notasLabel.setGeometry(150, 70, 140, 30)
-		self.notasLabel.show()
+        self.numCompasesCombo = QSpinBox(self)
+        self.numCompasesCombo.setGeometry(15, 490, 120, 30)
+        self.numCompasesCombo.setValue(2)
+        self.numCompasesCombo.setRange(1, 20)
 
-		self.notaLabel = QLabel(self)
-		self.notaLabel.setText("Nota")
-		self.notaLabel.setGeometry(150, 105, 50, 30)
-		self.notaLabel.show()
+        # READY #
+        self.btnReady = QPushButton("Ready", self)
+        self.btnReady.setGeometry(140, 10, 100, 45)
+        self.btnReady.clicked.connect(lambda: self.inputNotes())
 
-		self.octavaLabel = QLabel(self)
-		self.octavaLabel.setText("Octava")
-		self.octavaLabel.setGeometry(225, 105, 100, 30)
-		self.octavaLabel.show()
+        self.userNotification = QLabel(self)
+        self.userNotification.setText("Hay notas que no forman parte")
+        self.userNotification.setGeometry(310, 50, 200, 30)
+        self.userNotification.hide()
+        self.userNotificationTwo = QLabel(self)
+        self.userNotificationTwo.setText("de la escala")
+        self.userNotificationTwo.setGeometry(310, 70, 200, 30)
+        self.userNotificationTwo.hide()
 
-		for note in range(self.numNotasCombo.value()):
-			globals()[f"key_{note}"] = QComboBox(self)
-			globals()[f"key_{note}"].setGeometry(140, yInitChecBox, 70, 30)
+    def clear(self):
+        for object in self.notas:
+            object.hide()
+        for object in self.octavas:
+            object.hide()
 
-			for key in utilities.keys: 
-				globals()[f"key_{note}"].addItem(key)
+        self.notas.clear()
+        self.octavas.clear()
 
-			globals()[f"key_{note}"].show()
-			
-			globals()[f"octave_{note}"] = QSpinBox(self)
-			globals()[f"octave_{note}"].setGeometry(225, yInitChecBox, 60, 30)
-			globals()[f"octave_{note}"].setValue(4)
-			globals()[f"octave_{note}"].setRange(-1, 8)
-			globals()[f"octave_{note}"].show()
-			
-			self.notas.append(globals()[f"key_{note}"])
-			self.octavas.append(globals()[f"octave_{note}"])
-			yInitChecBox+=30
-		
-		# READY #
-		self.btnCreate = QPushButton('Create', self)
-		self.btnCreate.setGeometry(300, 10, 100, 45)
-		self.btnCreate.clicked.connect(lambda: self.midiProcessing())
-		self.btnCreate.show()
+    def inputNotes(self):
+        self.userNotification.hide()
+        self.userNotificationTwo.hide()
 
-	def getMidiNotesFromGUI(self):
-		for i in range(len(self.notas)):
-			stringNota = str(self.notas[i].currentText()) + str(self.octavas[i].value())
-			self.notasEnMidi.append(utilities.getMidiFromNote(stringNota))
+        self.clear()
+        yInitChecBox = 130
 
-	def midiProcessing(self):
-		self.getMidiNotesFromGUI()
-		distance = self.distanceCombo.value()
+        self.notasLabel = QLabel(self)
+        self.notasLabel.setText("Notas fundamentales")
+        self.notasLabel.setGeometry(150, 70, 140, 30)
+        self.notasLabel.show()
 
-		midiFile = MIDIFile(1)
-		midiFile.addTempo(0, 0, self.bpm.value())
+        self.notaLabel = QLabel(self)
+        self.notaLabel.setText("Nota")
+        self.notaLabel.setGeometry(150, 105, 50, 30)
+        self.notaLabel.show()
 
-		# CALCULATE NOTES NUMBER PER COMPAS
-		tiempo = 0
-		duration = utilities.getDurationFromNotation(self.rateCombo.currentText())
-		# NUMERO DE NOTAS POR COMPAS
-		intervalTimes = 4/duration
+        self.octavaLabel = QLabel(self)
+        self.octavaLabel.setText("Octava")
+        self.octavaLabel.setGeometry(225, 105, 100, 30)
+        self.octavaLabel.show()
 
-		# GET FULL MIDI SCALE
-		# GET LOWEST FUNDAMENTAL MIDI NOTE (C-1)
-		fundamentalMidi = utilities.getMidiFromNote(self.keyCombo.currentText() + "-1")
-		# CREAR ESCALA MAYOR O MENOR COMPLETA DE 0 A 127
-		scale = utilities.createScaleArray(self.modoCombo.currentText(), fundamentalMidi)
-		# GET SUM FOR UP OR DOWN
-		distance *= utilities.getDistanciaSign(self.orderCombo.currentText())
+        for note in range(self.numNotasCombo.value()):
+            globals()[f"key_{note}"] = QComboBox(self)
+            globals()[f"key_{note}"].setGeometry(140, yInitChecBox, 70, 30)
 
-		# FLAG FOR MIDI FILE
-		midiFileIsDone = False
-		inicio = 0
-		# GENERATE MIDI DATA
-		for i in range(int(self.numNotasCombo.value())):
-			nota = self.notasEnMidi[i]
+            for key in utilities.keys:
+                globals()[f"key_{note}"].addItem(key)
 
-			try:
-				inicio = scale.index(nota)
-				midiFileIsDone = True
-			except:
-				print("Hay notas que no forman parte de la escala")
-				self.userNotification.show()
-				self.userNotificationTwo.show()
-				midiFileIsDone = False
-				break
+            globals()[f"key_{note}"].show()
 
-			index = inicio
+            globals()[f"octave_{note}"] = QSpinBox(self)
+            globals()[f"octave_{note}"].setGeometry(225, yInitChecBox, 60, 30)
+            globals()[f"octave_{note}"].setValue(4)
+            globals()[f"octave_{note}"].setRange(-1, 8)
+            globals()[f"octave_{note}"].show()
 
-			contNota = 0
-			for j in range(int(intervalTimes) * int(self.numCompasesCombo.value())):
-				utilities.addNote(midiFile, scale[index], utilities.randomVelocity(1), tiempo, duration)
-				tiempo += duration
-				index += distance
-				contNota += 1
+            self.notas.append(globals()[f"key_{note}"])
+            self.octavas.append(globals()[f"octave_{note}"])
+            yInitChecBox += 30
 
-				if contNota >= (self.stepsCombo.value()+1):
-					index = scale.index(nota)
-					contNota = 0
+        # READY #
+        self.btnCreate = QPushButton("Create", self)
+        self.btnCreate.setGeometry(300, 10, 100, 45)
+        self.btnCreate.clicked.connect(lambda: self.midiProcessing())
+        self.btnCreate.show()
 
-			#for j in range(int(self.stepsCombo.value())+1):
-				#utilities.addNote(midiFile, scale[index], utilities.randomVelocity(1), tiempo, duration)
-				#tiempo += duration
-				#index += distance
+    def getMidiNotesFromGUI(self):
+        for i in range(len(self.notas)):
+            stringNota = str(self.notas[i].currentText()) + str(self.octavas[i].value())
+            self.notasEnMidi.append(utilities.getMidiFromNote(stringNota))
 
-		if midiFileIsDone:
-			with open("MidiArpeggiator/Midis/Arp.mid", "wb") as myOutputMIDIClip:
-				midiFile.writeFile(myOutputMIDIClip)
+    def midiProcessing(self):
+        self.getMidiNotesFromGUI()
+        distance = self.distanceCombo.value()
 
-			print("-------------------")
-			print("Archivo MIDI generado")
-			print("-------------------")
+        midiFile = MIDIFile(1)
+        midiFile.addTempo(0, 0, self.bpm.value())
 
-			pm = PrettyMIDI("MidiArpeggiator/Midis/Arp.mid")
-			plotter = Plotter()
-			plotter.show(pm, "/tmp/example-01.html")
+        # CALCULATE NOTES NUMBER PER COMPAS
+        tiempo = 0
+        duration = utilities.getDurationFromNotation(self.rateCombo.currentText())
+        # NUMERO DE NOTAS POR COMPAS
+        intervalTimes = 4 / duration
+
+        # GET FULL MIDI SCALE
+        # GET LOWEST FUNDAMENTAL MIDI NOTE (C-1)
+        fundamentalMidi = utilities.getMidiFromNote(self.keyCombo.currentText() + "-1")
+        # CREAR ESCALA MAYOR O MENOR COMPLETA DE 0 A 127
+        scale = utilities.createScaleArray(
+            self.modoCombo.currentText(), fundamentalMidi
+        )
+        # GET SUM FOR UP OR DOWN
+        distance *= utilities.getDistanciaSign(self.orderCombo.currentText())
+
+        # FLAG FOR MIDI FILE
+        midiFileIsDone = False
+        inicio = 0
+        # GENERATE MIDI DATA
+        for i in range(int(self.numNotasCombo.value())):
+            nota = self.notasEnMidi[i]
+
+            try:
+                inicio = scale.index(nota)
+                midiFileIsDone = True
+            except:
+                print("Hay notas que no forman parte de la escala")
+                self.userNotification.show()
+                self.userNotificationTwo.show()
+                midiFileIsDone = False
+                break
+
+            index = inicio
+
+            contNota = 0
+            for j in range(int(intervalTimes) * int(self.numCompasesCombo.value())):
+                utilities.addNote(
+                    midiFile,
+                    scale[index],
+                    utilities.randomVelocity(1),
+                    tiempo,
+                    duration,
+                )
+                tiempo += duration
+                index += distance
+                contNota += 1
+
+                if contNota >= (self.stepsCombo.value() + 1):
+                    index = scale.index(nota)
+                    contNota = 0
+
+        if midiFileIsDone:
+            with open("Midis/Arp.mid", "wb") as myOutputMIDIClip:
+                midiFile.writeFile(myOutputMIDIClip)
+
+            print("-------------------")
+            print("Archivo MIDI generado")
+            print("-------------------")
+
+            pm = PrettyMIDI("Midis/Arp.mid")
+            plotter = Plotter()
+            plotter.show(pm, "/tmp/example-01.html")
+
 
 app = QApplication(sys.argv)
 demo = Main()
--- utilities.py
+++ utilities.py
@@ -3,16 +3,17 @@ import random
 
 ################################################## GENERAL DATA ################################################
 ## Note durations
-durations = {	"whole note"     			:64,  
-				"dotted white note"     	:48,   
-				"white note"     			:32, 
-				"dotted quarter note"		:24,   
-				"quarter note"				:16, 
-				"dotted eight note"     	:12,
-				"eight note"     			:8,  
-				"dotted sixteenth note" 	:6,    
-				"sixteenth note" 			:4   
-            }
+durations = {
+    "whole note": 64,
+    "dotted white note": 48,
+    "white note": 32,
+    "dotted quarter note": 24,
+    "quarter note": 16,
+    "dotted eight note": 12,
+    "eight note": 8,
+    "dotted sixteenth note": 6,
+    "sixteenth note": 4,
+}
 
 ## KEYS
 keys = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
@@ -28,50 +29,57 @@ minorScale = [2, 1, 2, 2, 1, 2, 2]
 for i in range(-1, 10):
     for note in keys:
         if midiNoteNumber <= 127:
-            noteKey = "%s%s"%(note, i)
+            noteKey = "%s%s" % (note, i)
             notes[noteKey] = midiNoteNumber
-        midiNoteNumber+=1
+        midiNoteNumber += 1
+
 
 ################################################## FUNCTIONS ################################################
 def addNote(inMidiFile, inMidiNote, inVelocity, start_time, note_duration):
     inMidiFile.addNote(0, 0, inMidiNote, start_time, note_duration, inVelocity)
 
+
 def randomVelocity(inTrigger):
     if int(inTrigger) == 0:
         return 0
     else:
-        return (random.randint(60, 127))
+        return random.randint(60, 127)
+
 
 def getMidiFromNote(inNote):
-	return int(notes[inNote])
+    return int(notes[inNote])
+
 
 def split(word):
     return [char for char in word]
 
+
 def getDurationFromNotation(inNotation):
-	return durations[inNotation]/16
+    return durations[inNotation] / 16
+
 
 def getDistanciaSign(inMode):
-	if inMode == "up":
-		return 1
-	else:
-		return -1
+    if inMode == "up":
+        return 1
+    else:
+        return -1
+
 
 def createScaleArray(inMode, inFundamentalMidi):
-	if inMode == "mayor":
-		scaleLocal = majorScale
-	else:
-		scaleLocal = minorScale
-	
-	cont = 0
-	currentNote = inFundamentalMidi
-	scale = []
-	for i in range(inFundamentalMidi, 127): 
-		if currentNote <= 127:
-			currentNote += scaleLocal[cont]
-			scale.append(currentNote)
-			cont += 1
-			if cont >= len(scaleLocal):
-				cont = 0
+    if inMode == "mayor":
+        scaleLocal = majorScale
+    else:
+        scaleLocal = minorScale
 
-	return scale
+    cont = 0
+    currentNote = inFundamentalMidi
+    scale = []
+    for i in range(inFundamentalMidi, 127):
+        if currentNote <= 127:
+            currentNote += scaleLocal[cont]
+            scale.append(currentNote)
+            cont += 1
+            if cont >= len(scaleLocal):
+                cont = 0
+
+    return scale
